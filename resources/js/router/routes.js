1b53f462b353e502ba049f91c20521cfccaddfe8
benlittlenz <benlittle53@gmail.com>
2020-12-29 11:44:08 +1300

    fetching transactions on load

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -4,7 +4,7 @@ function page (path) {
 
 export default [
   { path: '/', name: 'welcome', component: page('welcome.vue') },
-  { path: '/hey', name: 'welcome', component: page('table.vue') },
+  { path: '/hey', name: 'hey', component: page('table.vue') },
 
   { path: '/login', name: 'login', component: page('auth/login.vue') },
   { path: '/register', name: 'register', component: page('auth/register.vue') },
