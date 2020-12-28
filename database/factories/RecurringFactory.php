38877a0b197e41e11d5a4a0eaf22086e0c85f261
benlittlenz <ben@pop-os.localdomain>
2020-12-29 11:09:24 +1300

    eated migrations and models

---
+++ database/factories/RecurringFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Recurring;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RecurringFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Recurring::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
