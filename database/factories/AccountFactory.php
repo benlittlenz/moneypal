5c7e94b35d344bb6fabe5fe7563a955689aa4f49
benlittlenz <benlittle53@gmail.com>
2021-01-01 10:06:00 +1300

    modified accounts factory

--- database/factories/AccountFactory.php
+++ database/factories/AccountFactory.php
@@ -31,12 +31,34 @@ class AccountFactory extends Factory
                 "Cryptocurrency",
                 "Other"
             )),
+            'sub_type' => $this->faker->randomElement(array(
+                "Checkings",
+                "Savings",
+                "Cash",
+                "Credit Card",
+                "Retirement",
+                "Brokerage",
+                "Mortage",
+                "Student Loan",
+                "Car Loan",
+                "Line of Credit",
+                "Residence",
+                "Investment Property",
+                "Vacation Property",
+                "Commerical",
+                "Land",
+                "Car",
+                "Motorbike",
+                "Boat",
+                "Scooter",
+            )),
             'account_name' => $this->faker->randomElement(array(
                 "BNZ",
                 "ANZ",
                 "Westpac",
             )),
             'bank_name' => $this->faker->word,
+            'balance' => $this->faker->randomFloat(2, 500, 5000)
         ];
     }
 }
