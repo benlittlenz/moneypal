a1debf299693c1960a3911f400d9da76e09d8990
benlittlenz <ben@pop-os.localdomain>
2021-01-02 18:12:29 +1300

    added controller and migration for tags

---
+++ resources/js/store/modules/tags.js
@@ -0,0 +1,65 @@
+import axios from 'axios'
+import * as types from '../mutation-types'
+
+export const state = {
+    tags: []
+}
+
+//getters
+export const getters = {
+    tags: state => state.tags,
+}
+
+// mutations
+export const mutations = {
+    [types.FETCH_TAGS](state, { tags }) {
+        state.tags = tags
+    },
+    // [types.CREATE_CATEGORY](state, { data }) {
+    //     state.tags.data.push({ data })
+    // },
+
+    // [types.UPDATE_CATEGORY] (state, data ) {
+    //     console.log('state', state)
+    //     console.log('data', data)
+    //     // state.categories.data = data
+
+    //     // console.log("cat id: ", state.category.data.id)
+    //     // console.log("data id: ", data.id)
+    //     state.categories.data = state.categories.data.map(category => {
+    //         if (category.id === data.id) {
+    //           return Object.assign({}, category, data)
+    //         }
+    //         return category
+    //       })
+    // }
+}
+
+// actions
+export const actions = {
+    async fetchTags({ commit }) {
+        try {
+            const { data } = await axios.get('/api/tags')
+            console.log(data)
+
+            commit(types.FETCH_TAGS, { categories: data })
+        } catch (e) {
+            console.log("ERROR", e)
+            //commit(types.FETCH_USER_FAILURE)
+        }
+    },
+    // async createCategory({ commit }, { data }) {
+    //     console.log("DATA", data)
+    //     commit(types.CREATE_CATEGORY, { data })
+
+    //     return await axios.post('/api/categories', data)
+    // },
+
+    // async updateCategory({ commit }, payload) {
+    //     const categoryID = payload.id
+    //     console.log("ID: ", categoryID)
+    //     console.log(payload)
+    //     commit(types.UPDATE_CATEGORY, payload)
+    //     return await axios.patch(`/api/categories/${categoryID}`, payload.data)
+    // },
+}
