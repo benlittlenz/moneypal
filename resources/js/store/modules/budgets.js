24d58ecda1edab7a39d15151a3c5dc976bc522ee
benlittlenz <ben@pop-os.localdomain>
2021-01-07 10:58:36 +1300

    fetching budgets from API

---
+++ resources/js/store/modules/budgets.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import * as types from '../mutation-types'
+
+export const state = {
+    budgets: []
+}
+
+//getters
+export const getters = {
+    budgets: state => state.budgets,
+}
+
+// mutations
+export const mutations = {
+    [types.FETCH_BUDGETS](state, { budgets }) {
+        state.budgets = budgets
+    },
+    // [types.UPDATE_CATEGORY] (state, data ) {
+    //     console.log('state', state)
+    //     console.log('data', data)
+    //     // state.categories.data = data
+
+    //     // console.log("cat id: ", state.category.data.id)
+    //     // console.log("data id: ", data.id)
+    //     state.categories.data = state.categories.data.map(category => {
+    //         if (category.id === data.id) {
+    //           return Object.assign({}, category, data)
+    //         }
+    //         return category
+    //       })
+    // }
+}
+
+// actions
+export const actions = {
+    async fetchBudgets({ commit }, {month, year}) {
+        try {
+            console.log("month: ", month)
+            console.log("year: ", year)
+            const { data } = await axios.get(`/api/budgets/${month}/${year}`)
+            console.log(data)
+
+            commit(types.FETCH_BUDGETS, { budgets: data })
+        } catch (e) {
+            console.log("ERROR", e)
+            //commit(types.FETCH_USER_FAILURE)
+        }
+    },
+    // async updateBudget({ commit }, payload) {
+    //     const categoryID = payload.id
+    //     console.log("ID: ", categoryID)
+    //     console.log(payload)
+    //     commit(types.UPDATE_CATEGORY, payload)
+    //     return await axios.patch(`/api/categories/${categoryID}`, payload.data)
+    // },
+}
